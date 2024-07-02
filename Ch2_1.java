9de2fc41321484a47cacf78754c3275a55f9a7db
Yuan8aexx <yuannchen1116@gmail.com>
2024-07-02 13:55:30 +0800

    123

---
+++ Ch2_1.java
@@ -0,0 +1,18 @@
+public class Ch2_1 {//定義public類別Ch2_1
+    public class void main(String[]args){//main函數，主程式開始
+        int num1=2;//宣告整數num
+        int num2=3;
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are running");//印出字串及變數內容
+        System.out.println(num2+"dogs are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"tigers are running");
+    }
+    }
+
+    }
+
+    }
+    
+}

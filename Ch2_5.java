841e6971ce0f9cedcb8e1a06f78bae0caca4bb28
Yuan8aexx <yuannchen1116@gmail.com>
2024-07-03 11:40:09 +0800

    1123

---
+++ Ch2_5.java
@@ -0,0 +1,9 @@
+public class Ch2_5 {
+    public static void main(String[] args) {
+        int a=2;  //宣告整數變數a，並設值為2
+        int b=3;  //宣告整數變數b，並設值為3
+        int c=a+b;  //宣告變數a+b為c
+
+        System.out.println("a+b="+c);  //印出結果
+    }
+}

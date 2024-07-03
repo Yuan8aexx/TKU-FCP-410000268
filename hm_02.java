ae7e4ea0910f3b20ce76d66a445b23cbd6f005c5
Yuan8aexx <yuannchen1116@gmail.com>
2024-07-03 14:59:41 +0800

    0703

---
+++ hm_02.java
@@ -0,0 +1,8 @@
+public class hm_02 {
+    public static void main(String[] args) {
+        
+        System.out.println("姓名:陳俞諳");
+        System.out.println("年齡:21");
+        System.out.println("興趣愛好:打遊戲");
+    }
+}
---
+++ hm_05.java
@@ -0,0 +1,19 @@
+public class hm_05 {
+    public static void main(String[] args) {
+        int a=12;
+        int b=7;
+        
+        int sum=a+b;
+        int difference=a-b;
+        int product=a*b;
+        int quotient=a/b;
+        int remainder=a%b; 
+
+        System.out.println("和: " + sum);
+        System.out.println("差: " + difference);
+        System.out.println("積: " + product);
+        System.out.println("商: " + quotient);
+        System.out.println("餘: "+remainder);
+
+    }
+}
